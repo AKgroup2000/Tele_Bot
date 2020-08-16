56d6a563817d08c7be67c1012a062887b76d37cf
AKgroup2000 <65353307+AKgroup2000@users.noreply.github.com>
2020-08-16 21:07:15 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,35 +2,46 @@ from Auto_chat import Chatbot
 from firebase import firebase
 firebase = firebase.FirebaseApplication("https://just-49431.firebaseio.com/", None)
 res=firebase.get('/just-49431/data/First_table','')
-for a in res:
+print(res)
+reply=[]
+'''for a in res:
 	#print(a)
 	b= firebase.get('/just-49431/data/First_table','{}'.format(a))
-	print(b['Email'])
+	print(b['Email'])'''
 Auto_chat=Chatbot("config.cfg")
-
+app=2
 def make_reply(msg):
-    reply = None
-    if msg is not None:
-		if msg=='Mobile':
-			for a in res :
+	reply = []
+	if msg is not None:
+		print(msg, "\t", type(msg))
+		if msg == '/Mobile':
+			for a in res:
+				print("a = ",a)
 				b= firebase.get('/just-49431/data/First_table','{}'.format(a))
-				reply=b['Email']
+				print("\n",b,"\t",type(b['Email']),"\n")
+				tem=str(b['Email'])
+				print(type(tem))
+				reply.append(tem)
+			print(reply)
 
-    	else:
-        	reply = "Welcome Telegram Chatbot"
-    return reply
+		else:
+			reply = "Welcome Telegram Chatbot"
+	return reply
 
 update_id = None
 while True:
-    updates = Auto_chat.get_updates(offset=update_id)
-    updates = updates["result"]
-    if updates:
-        for item in updates:
-            update_id = item["update_id"]
-            try:
-                message = str(item["message"]["text"])
-            except:
-                message = None
-            from_ = item["message"]["from"]["id"]
-            reply = make_reply(message)
-            Auto_chat.send_message(reply, fro
+	updates = Auto_chat.get_updates(offset=update_id)
+	updates = updates["result"]
+	if updates:
+		for item in updates:
+			update_id = item["update_id"]
+			try:
+				message = str(item["message"]["text"])
+			except:
+				message = None
+			from_ = item["message"]["from"]["id"]
+			reply = make_reply(message)
+			index=0
+			for a in res:
+				Auto_chat.send_message(reply[index], from_)
+				index+=1
